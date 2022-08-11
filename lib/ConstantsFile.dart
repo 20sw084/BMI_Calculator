73fbf567d3c6e45041240367a55231e30645998d
Junaid Aslam <junaidaslam.muet@gmail.com>
2022-08-11 14:13:59 +0500

    2nd commit

--- lib/ConstantsFile.dart
+++ lib/ConstantsFile.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-const activeColor = Color(0xFF1D1E33);
+const activeColor = Color(0xFF8D8E98);
 const deActiveColor = Color(0xFF111328);
 
 const kLabelStyle = TextStyle(
   fontSize: 30.0,
-  color: Color(0xFF8D8E98),
+  color: Colors.white,
 );
 
 const kSliderWidgetTextStyle = TextStyle(
